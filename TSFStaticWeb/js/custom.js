b6c8ce68b2297a8d93ed241b2ca6bc2ff1a4b164
DESKTOP-N99D9G7\Aditya Sapate <adisaps8@gmail.com>
2018-04-18 15:14:03 +0530

    Static Websites

---
+++ TSFStaticWeb/js/custom.js
@@ -0,0 +1,20 @@
+(function ($) {
+                
+    // Navigation scrolls
+    $(".navbar-nav li a").on('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+        $('html, body').stop().animate({				
+            scrollTop: $($anchor.attr('href')).offset().top				
+        }, 1500, 'easeInOutExpo');
+        
+        event.preventDefault();
+        }
+    });
+    
+   
+    
+})(jQuery);
